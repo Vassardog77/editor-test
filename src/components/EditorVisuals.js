fb41130474488e847e27600f4f09c81d699cbe63
Vassardog77 <benmoxon256@gmail.com>
2023-12-13 22:30:00 -0600

    screenshot

--- src/components/EditorVisuals.js
+++ src/components/EditorVisuals.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import html2canvas from 'html2canvas';
 import Textbox from "./Textbox";
 import ImageComponent from "./Image"; 
 import ResizableImage from "./Custom Components/ResizableImage";
@@ -8,7 +9,6 @@ import DraggableTextbox from "./Custom Components/DraggableTextbox";
 
 function EditorVisuals() {
   const [elements, setElements] = useState([]);
-
   const fileInputRef = React.useRef(null);
 
   const handleImageUpload = (e) => {
@@ -27,6 +27,35 @@ function EditorVisuals() {
     setElements(newElements);
   };
 
+  const saveImage = () => {
+    const displayArea = document.querySelector('.display-area');
+    const resizableElements = document.querySelectorAll('.resizable-element');
+    const resizeHandles = document.querySelectorAll('.resize-handle');
+    const rotateHandles = document.querySelectorAll('.rotate-handle');
+
+    // Hide resize and rotate handles
+    resizeHandles.forEach(handle => handle.style.display = 'none');
+    rotateHandles.forEach(handle => handle.style.display = 'none');
+
+    html2canvas(displayArea).then((canvas) => {
+      // Show resize and rotate handles again
+      resizeHandles.forEach(handle => handle.style.display = '');
+      rotateHandles.forEach(handle => handle.style.display = '');
+
+      const image = canvas.toDataURL('image/png', 1.0);
+      downloadImage(image, 'my-canvas.png');
+    });
+  };
+
+  const downloadImage = (blob, filename) => {
+    const fakeLink = window.document.createElement('a');
+    fakeLink.href = blob;
+    fakeLink.download = filename;
+    document.body.appendChild(fakeLink);
+    fakeLink.click();
+    document.body.removeChild(fakeLink);
+  };
+
   return (
     <div className="background">
       <div className="top-bar">
@@ -39,18 +68,19 @@ function EditorVisuals() {
         />
         <button onClick={() => fileInputRef.current.click()}>Import Image</button>
         <button onClick={addTextBox}>Add Text Box</button>
+        <button onClick={saveImage}>Save as Image</button>
       </div>
       <div className="display-area">
         {elements.map((element, index) =>
           element.type === "image" ? (
             <DraggableImages key={index}>
-                <ResizableImage src={element.src}>
+                <ResizableImage className="resizable-element" src={element.src}>
                     <ImageComponent src={element.src} />
                 </ResizableImage>
             </DraggableImages>
           ) : (
             <DraggableTextbox key={index}>
-            <ResizableTextbox>
+            <ResizableTextbox className="resizable-element">
               <Textbox
                 initialText={element.text}
                 onTextChange={(newText) => handleTextChange(index, newText)}
